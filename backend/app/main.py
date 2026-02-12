22c51a30f633a7cf0ac030e1af157c673c62b883
Fernando Robles <luis.fer1407@gmail.com>
2026-02-11 22:30:10 -0600

    Se corrigio una coma en el archivo main, lo que bugueaba la aplicacion

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
     "http://localhost:5000",
     "http://127.0.0.1:5000"
     
