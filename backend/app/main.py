5fa01fbacf59f22ecf889bf155bbe4365da38b90
Fernando Robles <luis.fer1407@gmail.com>
2026-02-11 22:18:27 -0600

    Se modifico el main para que acepte peticiones desde el puerto 5173 y el 5000

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,9 +4,17 @@ from app.routers import products, inventory, stats, sales, sales_history, ml, da
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+    "http://localhost:5000",
+    "http://127.0.0.1:5000"
+    
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex="https?://.*",
+    allow_origins=origins,
     
     allow_credentials=True,
     allow_methods=["*"],
